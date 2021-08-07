a90d280ec182ff844791d54ef1f8bafa22e0a931
ayumi-tulips <snowcat.kuroneko@gmail.com>
2021-08-07 18:04:07 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/ayumi/apiapp/ApiFragment.kt
@@ -0,0 +1,6 @@
+package jp.techacademy.ayumi.apiapp
+
+import androidx.fragment.app.Fragment
+
+class ApiFragment: Fragment() {
+}
---
+++ app/src/main/java/jp/techacademy/ayumi/apiapp/FragmentStateAdapter.kt
@@ -0,0 +1,4 @@
+package jp.techacademy.ayumi.apiapp
+
+class FragmentStateAdapter {
+}
